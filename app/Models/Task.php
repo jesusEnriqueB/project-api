ec50c543aff3c18173a90652c5879c6fec19f4c7
Jesus Enrique Balam <jesus.balambatun@gmail.com>
2025-08-19 06:19:36 -0600

    feat(api): make migrations

---
+++ app/Models/Task.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['name','description','project_id','user_id','status','percentage'];
+
+    public function project() { return $this->belongsTo(Project::class); }
+    public function user() { return $this->belongsTo(User::class); }
+}
